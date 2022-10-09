250928caa90a5b69f6f32cc16fb3bc7841fcecfc
Rylan Pow <powrylan@gmail.com>
2022-10-09 03:47:42 -0700

    fixed bracket mismatch

--- src/App.tsx
+++ src/App.tsx
@@ -16,17 +16,17 @@ const App: React.FC = () => {
   console.log(grid); //will print in the console(obviously)
 
   //line DIRECTLY below turns single column of boxes into a full grid, `repeat... 2px` is surrounded by BACKTICKS!!!
-  return <div style = {{display: 'grid', gridTemplateColumns: `repeat(${numCols}, 20px)`}}> 
-    {grid.map((rows, i) => rows.map((col, k) => 
+  return (
+    <div style = {{display: 'grid', gridTemplateColumns: `repeat(${numCols}, 20px)`}}> 
+    {grid.map((rows, i) => rows.map((col, k) => ( 
     <div 
-
     key={`${i}-${k}`} //Arrays need this. Research further. This is a NONSTANDARD way to write a key.
 
     onClick={() => {
       const newGrid = produce(grid, gridCopy => {
         gridCopy[i][k] = grid[i][k] ? 0: 1; //if 0, click makes 1, and vice-versa
-      })
-      setGrid(newGrid)
+      });
+      setGrid(newGrid);
       //grid[i][k] = 1 UPDATING STATE VARIABLE LIKE THIS IS BAD! use "immer" library!
     }}
 
@@ -35,8 +35,12 @@ const App: React.FC = () => {
       height: 20, 
       backgroundColor: grid[i][k] ? 'pink' : undefined, //if grid[i][k] == 1, else leave "off"
       border: 'solid 1px black'
-  }} /> ))} 
+  }} 
+  /> 
+  ))
+    )}
   </div> //updates page everytime i save!
-};
+  );
+}
 
 export default App;
