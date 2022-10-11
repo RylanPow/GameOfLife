6a6d646e5b281a8e924e1dcbd9e93c701d702d66
Rylan Pow <powrylan@gmail.com>
2022-10-11 00:32:23 -0700

    fixed grid not running

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,32 @@ import produce from 'immer';
 const numRows = 50;
 const numCols = 50;
 
+const operations = [
+  [0, 1],
+  [0, -1],
+  [1, -1],
+  [-1, 1],
+  [1, 1],
+  [-1, -1],
+  [1, 0],
+  [-1, 0]
+];
+
+const generateEmptyGrid = () => {
+  const rows = [];
+  for (let i = 0; i < numRows; i++) {
+    rows.push(Array.from(Array(numCols), () => 0));
+  }
+
+  return rows;
+};
+
 const App: React.FC = () => {
   const [grid, setGrid] = useState(() => {
-    const rows = [];
-    for(let i = 0; i < numRows; i++) {
-      rows.push(Array.from(Array(numCols), () => 0))  
-    }
-    return rows;
-  }); //grid state stored here; values in grid will be changing obviously
+    return generateEmptyGrid();
+  });//grid state stored here; values in grid will be changing obviously
   
-  console.log(grid); //will print in the console(obviously)
+  //console.log(grid); //will print in the console(obviously)
 
   const [running, setRunning] = useState(false); //false by default
 
@@ -24,8 +40,34 @@ const App: React.FC = () => {
       if(!runningRef.current) {
         return;
       }
+      //this gridcopy below allows us to mutate grid
+      setGrid(g => {
+        return produce(g, gridCopy =>{
+          for(let i = 0; i < numRows; i++) {
+            for(let j = 0; j < numCols; j++)  {
+              let neighbors = 0;
+                neighbors +=1;
+                operations.forEach(([x, y]) => {
+                  const newI = i + x;
+                  const newJ = j + y;
+                  if(newI >= 0 && newI < numRows && newJ >= 0 && newJ < numCols) { //out of bounds checking
+                    neighbors += g[newI][newJ]
+                  }
+              });
+                //cell logic
+                if(neighbors < 2 || neighbors > 3) { 
+                  gridCopy[i][j] = 0;
+                } else if (g[i][j] === 0 && neighbors === 3) {
+                  gridCopy[i][j] = 1;
+                }
+              
+            }
+          }
+        });
+      });
+      
       setTimeout(runSimulation, 1000); //1000 ms delay
-  }, []) //empty array as second argument, to make sure function is only created once
+  }, []); //empty array as second argument, to make sure function is only created once
 
   //line DIRECTLY below turns single column of boxes into a full grid, `repeat... 2px` is surrounded by BACKTICKS!!!
   return (
@@ -33,26 +75,30 @@ const App: React.FC = () => {
     <> 
     <button onClick ={() =>{
       setRunning(!running);
+      if(!running) {
+      runningRef.current = true;
+      runSimulation();
+      }
     }}
     >
       {running ? 'stop' : 'start'}</button>
     <div style = {{display: 'grid', gridTemplateColumns: `repeat(${numCols}, 20px)`}}> 
-    {grid.map((rows, i) => rows.map((col, k) => ( 
+    {grid.map((rows, i) => rows.map((col, j) => ( 
     <div 
-    key={`${i}-${k}`} //Arrays need this. Research further. This is a NONSTANDARD way to write a key.
+    key={`${i}-${j}`} //Arrays need this. Research further. This is a NONSTANDARD way to write a key.
 
     onClick={() => {
       const newGrid = produce(grid, gridCopy => {
-        gridCopy[i][k] = grid[i][k] ? 0: 1; //if 0, click makes 1, and vice-versa
+        gridCopy[i][j] = grid[i][j] ? 0: 1; //if 0, click makes 1, and vice-versa
       });
       setGrid(newGrid);
-      //grid[i][k] = 1 UPDATING STATE VARIABLE LIKE THIS IS BAD! use "immer" library!
+      //grid[i][j] = 1 UPDATING STATE VARIABLE LIKE THIS IS BAD! use "immer" library!
     }}
 
     style = {{
       width: 20, 
       height: 20, 
-      backgroundColor: grid[i][k] ? 'pink' : undefined, //if grid[i][k] == 1, else leave "off"
+      backgroundColor: grid[i][j] ? 'pink' : undefined, //if grid[i][j] == 1, else leave "off"
       border: 'solid 1px black'
   }} 
   /> 
