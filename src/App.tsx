c95c11589e05d484b113098ea95dda7ead3a7625
Rylan Pow <powrylan@gmail.com>
2022-10-12 00:27:53 -0700

    fixed logic error, updated colors

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import React, {useCallback, useState, useRef} from 'react';
 import produce from 'immer';
 
-const numRows = 50;
+const numRows = 40;
 const numCols = 50;
+const cellSize = 20; //in px
 
 const operations = [
   [0, 1],
@@ -46,18 +47,17 @@ const App: React.FC = () => {
           for(let i = 0; i < numRows; i++) {
             for(let j = 0; j < numCols; j++)  {
               let neighbors = 0;
-                neighbors +=1;
                 operations.forEach(([x, y]) => {
                   const newI = i + x;
                   const newJ = j + y;
                   if(newI >= 0 && newI < numRows && newJ >= 0 && newJ < numCols) { //out of bounds checking
-                    neighbors += g[newI][newJ]
+                    neighbors += g[newI][newJ];
                   }
               });
                 //cell logic
                 if(neighbors < 2 || neighbors > 3) { 
                   gridCopy[i][j] = 0;
-                } else if (g[i][j] === 0 && neighbors === 3) {
+                } else if(g[i][j] == 0 && neighbors == 3) {
                   gridCopy[i][j] = 1;
                 }
               
@@ -93,7 +93,7 @@ const App: React.FC = () => {
     <button onClick={() => { //generate random grid
       const rows = [];
       for(let i = 0; i < numRows; i++) {
-        rows.push(Array.from(Array(numCols), () => (Math.random() - 0.7 > Math.random() ? 1 : 0)));
+        rows.push(Array.from(Array(numCols), () => (Math.random() - 0.35 > Math.random() ? 1 : 0)));
       }
       setGrid(rows);
     }}> Random </button>
@@ -107,17 +107,18 @@ const App: React.FC = () => {
 
     onClick={() => {
       const newGrid = produce(grid, gridCopy => {
-        gridCopy[i][j] = grid[i][j] ? 0: 1; //if 0, click makes 1, and vice-versa
+        gridCopy[i][j] = grid[i][j] ? 0 : 1; //if 0, click makes 1, and vice-versa
       });
       setGrid(newGrid);
       //grid[i][j] = 1 UPDATING STATE VARIABLE LIKE THIS IS BAD! use "immer" library!
     }}
 
     style = {{
-      width: 20, 
-      height: 20, 
-      backgroundColor: grid[i][j] ? 'blue' : undefined, //if grid[i][j] == 1, else leave "off"
-      border: 'solid 1px black'
+      width: cellSize, 
+      height: cellSize, 
+      backgroundColor: grid[i][j] ? 'skyblue' : undefined, //if grid[i][j] == 1, else leave "off"
+      border: 'solid 1px darkgray',
+
   }} 
   /> 
   ))
